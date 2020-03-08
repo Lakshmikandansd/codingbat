14e2622a64ed2eed6d0665c96d8c44fb0731304e
Lakshmikandansd <61616008+Lakshmikandansd@users.noreply.github.com>
2020-03-08 12:19:43 +0530

    Update linearIn.java

--- java/array-3/linearIn.java
+++ java/array-3/linearIn.java
@@ -2,23 +2,24 @@
  * return true if all of the numbers in inner appear in outer. The best 
  * solution makes only a single "linear" pass of both arrays, taking 
  * advantage of the fact that both arrays are already in sorted order.
- */
-public boolean linearIn(int[] outer, int[] inner) {
-    int i = 0;
-    int j = 0;
-      
-    while(i < inner.length && j < outer.length) {
-        if(inner[i] > outer[j]) {
-            j++;
-        } else if(inner[i] < outer[j]) {
-            return false;
-        } else {
-            i++;
-        }
+ */public boolean linearIn(int[] outer, int[] inner) {
+  int count=0;
+  for(int i=0;i<inner.length;i++)
+  {
+    int num=inner[i];
+    for(int j=i;j<outer.length;j++)
+    {
+      if(num==outer[j])
+      {
+        count++;
+        break;
+      }
     }
-                                              
-    if(i != inner.length)
-        return false;
-                                                      
-    return true;
+  }
+  if(count==inner.length)
+  {
+  return true;
+  }
+  return false;
 }
+
